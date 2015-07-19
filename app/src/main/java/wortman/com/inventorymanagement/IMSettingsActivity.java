3b86074db610995dac2feda3e13659e662577f1b
Bob Boatwright <boatwr01@email.franklin.edu>
2015-07-19 18:01:00 -0400

    Settings screen connected to device.  Help screen in progress.

--- app/src/main/java/wortman/com/inventorymanagement/IMSettingsActivity.java
+++ app/src/main/java/wortman/com/inventorymanagement/IMSettingsActivity.java
@@ -1,13 +1,21 @@
 package wortman.com.inventorymanagement;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import wortman.com.openshiftapplication.R;
 
@@ -26,6 +34,30 @@ public class IMSettingsActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.inv_man);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        Switch wirelessSwitch = (Switch)findViewById(R.id.wirelessSwitch);
+        wirelessSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    wifi.setWifiEnabled(true);
+                    Toast.makeText(IMSettingsActivity.this, "Wifi Enabled", Toast.LENGTH_SHORT).show();
+                } else {
+                    wifi.setWifiEnabled(false);
+                    Toast.makeText(IMSettingsActivity.this, "Wifi Disabled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button gpsButton = (Button) findViewById(R.id.gpsButton);
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
