d984b48c888f55f177cef07d5d2c74e331912572
Jason Edwards <edward86@email.franklin.edu>
2015-07-23 19:54:05 -0500

    UPC toast removed

--- app/src/main/java/wortman/com/inventorymanagement/IMBarcodeActivity.java
+++ app/src/main/java/wortman/com/inventorymanagement/IMBarcodeActivity.java
@@ -12,7 +12,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -86,10 +85,8 @@ public class IMBarcodeActivity extends ActionBarActivity {
         if(result != null) {
             if(result.getContents() == null) {
                 Log.d("MainActivity", "Cancelled scan");
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
                 Log.d("MainActivity", "Scanned");
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 Label = result.getContents().toString();
                 //GET INVENTORY ARRAY
                 new GetInventoryTask().execute(new ApiConnector());
